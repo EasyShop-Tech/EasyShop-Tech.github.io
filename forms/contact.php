3887dd16772cde3043ca7ca9d37c157f0933153b
EasyShop-Tech <62357413+EasyShop-Tech@users.noreply.github.com>
2020-04-11 15:04:41 +0530

    contact.php update

--- forms/contact.php
+++ forms/contact.php
@@ -1,18 +1,10 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'hello@easyshop.tech';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Unable send email(development in progress)');
   }
 
   $contact = new PHP_Email_Form;
@@ -23,7 +15,7 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  //SMTP to send emails
   /*
   $contact->smtp = array(
     'host' => 'example.com',
